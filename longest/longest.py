42444bac987effbecc3de59025e7e8b1607f15c5
hbelfer <hbelfer@gmail.com>
2022-12-18 14:20:44 +0200

    recursive

--- longest/longest.py
+++ longest/longest.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python
 
 
-
-def longest_str(string1:str,strings_list:list)->str:   
+def longest_recursive(string1:str,strings_list:list)->str: 
+    if(len(string1) == 0):
+        return ''
+        
     longest = ''
-    # print('string1: ',string1,' len: ',len(string1))
+    print('string1: ',string1,' len: ',len(string1),strings_list)
     for i in range(1,len(string1)+1):
         currentstr = string1[0:i]
         # print('i= ',i,'currentstr: ',currentstr )
         for s in range(len(strings_list)):
             if strings_list[s].find(currentstr) == -1:
-                return longest
+                longnext = longest_recursive(string1[1:len(string1)],strings_list)
+                return longnext if(len(longnext) > len(longest)) else longest
         longest = currentstr 
-    return longest
+    longnext = longest_recursive(string1[1:len(string1)],strings_list)
+    return longnext if(len(longnext) > len(longest)) else longest
 
-def longest_all(string2:str,strings_list:list)->str:
-    longlong = ''
-    st = string2
-    while(len(st) > 0):
-        sss = longest_str(st,strings_list)
-        if(len(sss) > len(longlong)):
-            longlong = sss
-        st = st[1:len(st)]
-    return longlong
     
 strings = ['rabcde','dabcdegr','rrrabcdeftr']
 st = strings[0]
-print(st,strings[1:len(strings)])
 
+print(st,strings[1:len(strings)])
+abc = longest_recursive(st,strings[1:len(strings)])
+print('abc=',abc)
+exit()
 long_long = longest_all(st,strings[1:len(strings)])
 print('the longets',long_long)
 

1830f6ad31b789d85d8995da70ae6d74eae08515
hbelfer <hbelfer@gmail.com>
2022-12-26 17:08:03 +0200

    bla

--- files/textfile.py
+++ files/textfile.py
@@ -6,12 +6,15 @@ import pathlib
 class TextFile:
     def __init__(self,path:str):
         self._path:str = path
-        self._dir_path:str = os.path.dirname(path)
         self._exist:bool = os.path.exists(path)
         self._suffix:str = pathlib.Path(path).suffix
-    
+        self._filename = os.path.basename(path) 
+        self._dir_path:str = self._path.replace(self._filename,'')
+        self.filename = self._filename.replace(".txt","").replace(".csv","").replace(".json","")
+        
+        
     def __str__(self):
-        return f'path: {self._path},exist: {self._exist},suffix: {self._suffix},dirpath: {self._dir_path}'
+        return f'path: {self._path},exist: {self._exist},suffix: {self._suffix},dirpath: {self._dir_path},filename {self.filename}'
     
     def __repr__(self):
         return f'TextFile({self._path})'
@@ -27,10 +30,5 @@ class TextFile:
             return -1
         self._file_handle = open(self._path,"rt")
 
-        
-
-abc = TextFile('c:\\temp\\noa\\pythoncourse\\files\\textfile.py')
-print(abc)   
-print(repr(abc))  
-print(abc.get_file_size()) 
+ 
 

60eb6b759a4f793e0469325d1ba3c8a42651224a
hbelfer <hbelfer@gmail.com>
2022-12-26 17:10:25 +0200

    bla

--- files/txtfile.py
+++ files/txtfile.py
@@ -20,22 +20,44 @@ class TxtFile(TextFile):
         st = self._file_handle.read()
         self._file_handle.close()
         return st
+    def suffix()->str:
+        return self._suffix
         
-    def __add__(self,path:str):
-        toadd = TxtFile(path)
+    def __add__(self,toadd):
         st = self.get_content() + toadd.get_content()
-        fname = self._dir_path + self.filename + '_' + toadd.filename + '.txt'
+        fname = self._dir_path+'\\'+ self.filename + '_' + toadd.filename + '.txt'
+        if os.path.exists(fname):
+            raise Exception(f'File {fname} exists!')
         fh = open(fname,"wt")
         fh.write(st)
         fh.close()
-        added = TextFile(fname)
+        added = TxtFile(fname)
         return added
         
-
-abc = TxtFile('c:\\temp\\noa\\pythoncourse\\files\\aaa.txt')
-print(abc)   
-print(repr(abc))  
-print('file size: ',abc.get_file_size()) 
-# print(abc.get_content())
-ddd = abc.__add__('c:\\temp\\noa\\pythoncourse\\files\\bbb.txt')
-print('file size: ',ddd.get_file_size()) 
+    def get_words_num(self)->int:
+        st = self.get_content()
+        stsplit = st.split(' ')
+        return len(stsplit)
+    
+    def get_avg_word_len(self)->float:
+        st = self.get_content()
+        stsplit = st.split(' ')
+        sum:float = 0
+        for word in stsplit:
+            sum += len(word)
+        return(sum/len(stsplit))
+            
+aaa = TxtFile('c:\\temp\\noa\\pythoncourse\\files\\aaa.txt')
+bbb = TxtFile('c:\\temp\\noa\\pythoncourse\\files\\bbb.txt')
+print('file size aaa ',aaa.get_file_size(),' bbb ',bbb.get_file_size()) 
+ccc = aaa +bbb
+print('type aaa ',type(aaa))
+print('type ccc ',type(ccc))
+print('file size ccc ',ccc.get_file_size())
+print('get_avg_word_len ',ccc.get_avg_word_len())
+print(ccc)   
+print(repr(ccc)) 
+print('num words: ', ccc.get_words_num())
+# # print(abc.get_content())
+# ddd = abc.__add__('c:\\temp\\noa\\pythoncourse\\files\\bbb.txt')
+# print('file size: ',ddd.get_file_size()) 
